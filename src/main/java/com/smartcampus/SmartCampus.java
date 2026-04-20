11b21e130d6bfdba214f08801d6d061d3a54ce80
User <nadilveenath524@gmail.com>
2026-04-20 13:22:16 +0530

    Initial project setup - Smart Campus API

---
+++ src/main/java/com/smartcampus/SmartCampus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.smartcampus;
+
+/**
+ *
+ * @author User
+ */
+public class SmartCampus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
