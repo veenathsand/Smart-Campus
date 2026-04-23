6321690ad81f199a642658b8165f1fff3ccf55fa
User <nadilveenath524@gmail.com>
2026-04-23 07:07:24 +0530

    Final submission - Smart Campus Web Application with Tomcat

--- src/main/java/com/smartcampus/SmartCampusApplication.java
+++ src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -1,10 +1,36 @@
 package com.smartcampus;
-
+ 
+import com.smartcampus.exception.mapper.GlobalExceptionMapper;
+import com.smartcampus.exception.mapper.LinkedResourceNotFoundExceptionMapper;
+import com.smartcampus.exception.mapper.RoomNotEmptyExceptionMapper;
+import com.smartcampus.exception.mapper.SensorUnavailableExceptionMapper;
+import com.smartcampus.filter.LoggingFilter;
+import com.smartcampus.resource.DiscoveryResource;
+import com.smartcampus.resource.RoomResource;
+import com.smartcampus.resource.SensorResource;
+ 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
+import java.util.HashSet;
+import java.util.Set;
+ 
 @ApplicationPath("/api/v1")
 public class SmartCampusApplication extends Application {
-    
-    // Jersey scans packages configured in Main.java
+ 
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        // Resources
+        classes.add(DiscoveryResource.class);
+        classes.add(RoomResource.class);
+        classes.add(SensorResource.class);
+        // Filters
+        classes.add(LoggingFilter.class);
+        // Exception Mappers
+        classes.add(GlobalExceptionMapper.class);
+        classes.add(LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(RoomNotEmptyExceptionMapper.class);
+        classes.add(SensorUnavailableExceptionMapper.class);
+        return classes;
+    }
 }
